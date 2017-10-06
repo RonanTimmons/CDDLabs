0d73b1369cf7fa3f85fbb9731e7d0894924190fa
RonanTimmons <napstimmons1888@gmail.com>
2017-10-06 09:29:18 +0100

    Lab 3 completed

---
+++ Lab3/Semaphore.h
@@ -0,0 +1,26 @@
+/*!
+@author Ronan Timmons
+@date 02/10/17
+@version 1.0
+ */
+
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+class Semaphore
+{
+private:
+    unsigned int m_uiCount;  /*!< Holds the Semaphore count */
+    std::mutex m_mutex;
+    std::condition_variable m_condition;
+
+public:
+    Semaphore(unsigned int uiCount=1)
+          : m_uiCount(uiCount) { };
+    void Wait();
+    template< typename R,typename P >
+    bool Wait(const std::chrono::duration<R,P>& crRelTime);
+    void Signal();
+
+};
---
+++ Lab3/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['semaphore',['Semaphore',['../d6/d72/classSemaphore.html',1,'']]]
+];
